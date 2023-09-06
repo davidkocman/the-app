6d1fc7329c6b71984bf7f7503ce22d55e47aa776
David Kocman <david.kocman@icloud.com>
2023-09-07 00:22:01 +0200

    sunrise/sunset

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,10 +1,12 @@
 // Capitalize Text
 // ----------------------------------
-const capitalize = (str: string): string => `${str.charAt(0).toUpperCase()}${str.slice(1)}`
+const capitalize = (str: string) => `${str.charAt(0).toUpperCase()}${str.slice(1)}`
 
 // Calculate Percent
 // ----------------------------------
-const calculatePercentage = (value: number, total: number): number => Math.round((value / total) * 100)
+const calculatePercentage = (value: number, total: number) => Math.round((value / total) * 100)
+
+const calculateValueFromPercentage = (percentage: number, total: number) => Math.round((percentage * total) / 100)
 
 // Remove Duplicate Elements from array
 // ----------------------------------
@@ -14,7 +16,9 @@ const removeDuplicates = (arr: Array<string | object>) => [...new Set(arr)]
 // ----------------------------------
 const sortBy = (arr: Array<object>, key: string) => {
   if (arr.length > 1) {
-    return arr.sort((a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0))
+    return arr.sort((a, b) =>
+      a[key as keyof object] > b[key as keyof object] ? 1 : a[key as keyof object] < b[key as keyof object] ? -1 : 0
+    )
   }
   console.log('Array has to have at least 2 object elements')
 }
@@ -25,19 +29,19 @@ const wait = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(
 
 // Extract data from an array of objects
 // ----------------------------------
-const extract = (objs: Array<object>, key: string) => objs.map((obj) => obj[key])
+const extract = (objs: Array<object>, key: string) => objs.map((obj) => obj[key as keyof object])
 
 // Check if a date is a weekday
 // ----------------------------------
-const isWeekday = (d: Date): boolean => d.getDay() % 6 !== 0
+const isWeekday = (d: Date) => d.getDay() % 6 !== 0
 
 // Check if an array is empty
 // ----------------------------------
-const isArrayEmpty = (arr: unknown[]): boolean => Array.isArray(arr) && !arr.length
+const isArrayEmpty = (arr: unknown[]) => Array.isArray(arr) && !arr.length
 
 // Check if an object/array is empty
 // ----------------------------------
-const isObjectEmpty = (obj: object): boolean => obj && Object.keys(obj).length === 0
+const isObjectEmpty = (obj: object) => obj && Object.keys(obj).length === 0
 
 // Group an array of objects by a key
 // ----------------------------------
@@ -51,23 +55,27 @@ const sortByKey = <T extends Record<string, any>, K extends keyof T>(arr: T[], k
 
 // Calculate the number of difference days between two dates
 // ----------------------------------
-const daysDiff = (date: Date, otherDate: Date): number =>
+const daysDiff = (date: Date, otherDate: Date) =>
   Math.ceil(Math.abs(date.valueOf() - otherDate.valueOf()) / (1000 * 60 * 60 * 24))
 
 // Calculate the number of months between two dates
 // ----------------------------------
-const monthDiff = (startDate: Date, endDate: Date): number =>
+const monthDiff = (startDate: Date, endDate: Date) =>
   Math.max(0, (endDate.getFullYear() - startDate.getFullYear()) * 12 - startDate.getMonth() + endDate.getMonth())
 
 // Get local time string from unix timestamp
 // ----------------------------------
-const timeFromUnix = (timestamp: number): string => new Date(timestamp * 1000).toLocaleTimeString('sk')
+const timeFromUnix = (timestamp: number, timezone: number) =>
+  `${new Date((timestamp + timezone) * 1e3).getHours() + new Date().getTimezoneOffset() / 60}:${String(
+    new Date((timestamp + timezone) * 1e3).getMinutes()
+  ).padStart(2, '0')}`
 
-const sunProgress = (sunrise: number, sunset: number, timezone: number): number | null => {
+const sunProgress = (sunrise: number, sunset: number, timezone: number) => {
   const time = new Date().getHours() * 60 + new Date().getMinutes() + timezone / 60
   const rise = new Date((sunrise + timezone) * 1e3).getHours() * 60 + new Date((sunrise + timezone) * 1e3).getMinutes()
   const set = new Date((sunset + timezone) * 1e3).getHours() * 60 + new Date((sunset + timezone) * 1e3).getMinutes()
   const currentTime = time >= 1440 ? time - 1440 : time
+  debugger
   if (currentTime >= rise && currentTime <= set) {
     const sunTimeDuration = set - rise
     return calculatePercentage(currentTime - rise, sunTimeDuration)
@@ -78,6 +86,7 @@ const sunProgress = (sunrise: number, sunset: number, timezone: number): number 
 export {
   capitalize,
   calculatePercentage,
+  calculateValueFromPercentage,
   removeDuplicates,
   sortBy,
   wait,
